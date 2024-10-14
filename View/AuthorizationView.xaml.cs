8c86c00586e88250c052988ab4a1abdf1838f770
DarthAkiraNihil <egorzverev666@gmail.com>
2024-10-14 17:39:35 +0300

    Initial commit

---
+++ View/AuthorizationView.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace GenosStore.View {
+	/// <summary>
+	/// Логика взаимодействия для AuthorizationView.xaml
+	/// </summary>
+	public partial class AuthorizationView : Window {
+		public AuthorizationView() {
+			InitializeComponent();
+		}
+
+		private void TextBox_TextChanged(object sender, TextChangedEventArgs e) {
+
+		}
+	}
+}
