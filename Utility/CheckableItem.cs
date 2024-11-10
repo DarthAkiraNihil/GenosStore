de29a6efeba04f1756e652fc7020292275277c48
DarthAkiraNihil <egorzverev666@gmail.com>
2024-11-10 14:18:17 +0300

    Add multi-choice filter support

---
+++ Utility/CheckableItem.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace GenosStore.Utility {
+    public class CheckableItem<T> {
+        public T Item { get; set; }
+        public bool IsChecked { get; set; }
+    }
+}
--- ViewModel/ItemList/MotherboardsListModel.cs
+++ ViewModel/ItemList/MotherboardsListModel.cs
@@ -1,17 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using GenosStore.Model.Context;
+using GenosStore.Model.Entity.Item.Characteristic;
 using GenosStore.Utility;
 
 namespace GenosStore.ViewModel.ItemList {
 	public class MotherboardsListModel: AbstractViewModel, INotifyPropertyChanged {
 		
 		private RelayCommand _toItemPageCommand;
+		private RelayCommand _applyFiltersCommand;
+		
+		public ObservableCollection<CheckableItem<MotherboardFormFactor>> MotherboardFormFactors { get; set; }
 		public RelayCommand ToItemPageCommand {
 			get { return _toItemPageCommand; }
+		}
+
+		public RelayCommand ApplyFiltersCommand {
+			get { return _applyFiltersCommand; }
 		}
 
 		private void ToItemPage(object parameter) {
@@ -19,6 +30,14 @@ namespace GenosStore.ViewModel.ItemList {
 		}
 
 		private bool CanToItemPage(object parameter) {
+			return true;
+		}
+
+		private void ApplyFilters(object parameter) {
+			MessageBox.Show("A");
+		}
+
+		private bool CanApplyFilters(object parameter) {
 			return true;
 		}
 
@@ -31,6 +50,15 @@ namespace GenosStore.ViewModel.ItemList {
 
 		public MotherboardsListModel() {
 			_toItemPageCommand = new RelayCommand(ToItemPage, CanToItemPage);
+			_applyFiltersCommand = new RelayCommand(ApplyFilters, CanApplyFilters);
+			var context = new GenosStoreDatabaseContext();
+			var l = context.MotherboardFormFactors.ToList();
+			MotherboardFormFactors = new ObservableCollection<CheckableItem<MotherboardFormFactor>>();
+			foreach (var v in l) {
+				MotherboardFormFactors.Add(
+					new CheckableItem<MotherboardFormFactor>() {Item = v, IsChecked = false}
+				);
+			}
 		}
 		
 	}
