ab3b719c01f852099728441c3c043d4bb8a41b61
DarthAkiraNihil <egorzverev666@gmail.com>
2024-11-10 15:00:34 +0300

    Motherboards page relayout

---
+++ Utility/Utilities.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using GenosStore.Model.Entity.Item.Characteristic;
+
+namespace GenosStore.Utility {
+    public class Utilities {
+        public static ObservableCollection<CheckableItem<T>> ConvertToCheckableCollection<T>(List<T> list) {
+            var converted = new ObservableCollection<CheckableItem<T>>();
+            foreach (var element in list) {
+                converted.Add(
+                    new CheckableItem<T>() {Item = element, IsChecked = false}
+                );
+            }
+            return converted;
+        }
+    }
+}
--- ViewModel/ItemList/MotherboardsListModel.cs
+++ ViewModel/ItemList/MotherboardsListModel.cs
@@ -8,15 +8,24 @@ using System.Threading.Tasks;
 using System.Windows;
 using GenosStore.Model.Context;
 using GenosStore.Model.Entity.Item.Characteristic;
+using GenosStore.Model.Entity.Item.ComputerComponent;
+using GenosStore.Model.Entity.Item.SimpleComputerComponent;
 using GenosStore.Utility;
 
 namespace GenosStore.ViewModel.ItemList {
 	public class MotherboardsListModel: AbstractViewModel, INotifyPropertyChanged {
 		
-		private RelayCommand _toItemPageCommand;
-		private RelayCommand _applyFiltersCommand;
+		private readonly RelayCommand _toItemPageCommand;
+		private readonly RelayCommand _applyFiltersCommand;
 		
+		public ObservableCollection<CheckableItem<Vendor>> Vendors { get; set; }
 		public ObservableCollection<CheckableItem<MotherboardFormFactor>> MotherboardFormFactors { get; set; }
+		public ObservableCollection<CheckableItem<CPUSocket>> CPUSockets { get; set; }
+		public ObservableCollection<CheckableItem<CPUCore>> CPUCores { get; set; }
+		public ObservableCollection<CheckableItem<RAMType>> RAMTypes { get; set; }
+		
+		public ObservableCollection<Motherboard> Motherboards { get; set; }
+		
 		public RelayCommand ToItemPageCommand {
 			get { return _toItemPageCommand; }
 		}
@@ -51,14 +60,21 @@ namespace GenosStore.ViewModel.ItemList {
 		public MotherboardsListModel() {
 			_toItemPageCommand = new RelayCommand(ToItemPage, CanToItemPage);
 			_applyFiltersCommand = new RelayCommand(ApplyFilters, CanApplyFilters);
+			
 			var context = new GenosStoreDatabaseContext();
-			var l = context.MotherboardFormFactors.ToList();
-			MotherboardFormFactors = new ObservableCollection<CheckableItem<MotherboardFormFactor>>();
-			foreach (var v in l) {
-				MotherboardFormFactors.Add(
-					new CheckableItem<MotherboardFormFactor>() {Item = v, IsChecked = false}
-				);
-			}
+
+			var m = new Motherboard();
+			
+			m.
+			
+			Vendors = Utilities.ConvertToCheckableCollection(context.Vendors.ToList());
+			MotherboardFormFactors = Utilities.ConvertToCheckableCollection(context.MotherboardFormFactors.ToList());
+			CPUSockets = Utilities.ConvertToCheckableCollection(context.CPUSockets.ToList());
+			CPUCores = Utilities.ConvertToCheckableCollection(context.CPUCores.ToList());
+			RAMTypes = Utilities.ConvertToCheckableCollection(context.RAMTypes.ToList());
+			MotherboardFormFactors = Utilities.ConvertToCheckableCollection(context.MotherboardFormFactors.ToList());
+			
+			
 		}
 		
 	}
