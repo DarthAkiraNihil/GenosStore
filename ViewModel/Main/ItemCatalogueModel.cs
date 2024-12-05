5151fc56251ac11ad1c97a26dd329f68aa8cd1fd
DarthAkiraNihil <egorzverev666@gmail.com>
2024-12-05 21:51:15 +0300

    Make all item list pages accessible

--- ViewModel/Main/ItemCatalogueModel.cs
+++ ViewModel/Main/ItemCatalogueModel.cs
@@ -26,7 +26,67 @@ namespace GenosStore.ViewModel.Main {
 				ViewModel = new MotherboardsListModel(s, u),
 				Title = "Материнские платы",
 				URL = "View/ItemList/MotherboardsPage.xaml"
-			}}
+			}},
+			{"CPUs", (s, u) => new ToItemListCommandParameters {
+				ViewModel = new CPUsListModel(s, u),
+				Title = "Процессоры",
+				URL = "View/ItemList/CPUsPage.xaml"
+			}},
+			{"RAMs", (s, u) => new ToItemListCommandParameters {
+				ViewModel = new RAMsListModel(s, u),
+				Title = "Оперативная память",
+				URL = "View/ItemList/RAMsPage.xaml"
+			}},
+			{"GraphicsCards", (s, u) => new ToItemListCommandParameters {
+				ViewModel = new GraphicsCardsListModel(s, u),
+				Title = "Видеокарты",
+				URL = "View/ItemList/GraphicCardsPage.xaml"
+			}},
+			{"HDDs", (s, u) => new ToItemListCommandParameters {
+				ViewModel = new HDDsListModel(s, u),
+				Title = "Жёсткие диски",
+				URL = "View/ItemList/HDDsPage.xaml"
+			}},
+			{"NVMeSSDs", (s, u) => new ToItemListCommandParameters {
+				ViewModel = new NVMeSSDsListModel(s, u),
+				Title = "Твердотельные накопители NVMe",
+				URL = "View/ItemList/NVMeSSDsPage.xaml"
+			}},
+			{"SataSSDs", (s, u) => new ToItemListCommandParameters {
+				ViewModel = new SataSSDsListModel(s, u),
+				Title = "Твердотельные накопители Sata",
+				URL = "View/ItemList/SataSSDsPage.xaml"
+			}},
+			{"CPUCoolers", (s, u) => new ToItemListCommandParameters {
+				ViewModel = new CPUCoolersListModel(s, u),
+				Title = "Кулеры для процессора",
+				URL = "View/ItemList/CPUCoolersPage.xaml"
+			}},
+			{"PowerSupplies", (s, u) => new ToItemListCommandParameters {
+				ViewModel = new PowerSuppliesListModel(s, u),
+				Title = "Блоки питания",
+				URL = "View/ItemList/PowerSuppliesPage.xaml"
+			}},
+			{"Displays", (s, u) => new ToItemListCommandParameters {
+				ViewModel = new DisplaysListModel(s, u),
+				Title = "Мониторы",
+				URL = "View/ItemList/DisplaysPage.xaml"
+			}},
+			{"Keyboards", (s, u) => new ToItemListCommandParameters {
+				ViewModel = new KeyboardsListModel(s, u),
+				Title = "Клавиатуры",
+				URL = "View/ItemList/KeyboardsPage.xaml"
+			}},
+			{"Mouses", (s, u) => new ToItemListCommandParameters {
+				ViewModel = new MousesListModel(s, u),
+				Title = "Мыши",
+				URL = "View/ItemList/MousesPage.xaml"
+			}},
+			{"PreparedAssemblies", (s, u) => new ToItemListCommandParameters {
+				ViewModel = new PreparedAssembliesListModel(s, u),
+				Title = "Готовые сборки",
+				URL = "View/ItemList/PreparedAssembliesPage.xaml"
+			}},
 		};
 
 		#region ToItemListCommand
